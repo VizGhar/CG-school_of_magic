ea39f64c954316be3fa04626e2a4c1e425a8e172
Ján Kandráč <jankandrac@Jans-iMac.local>
2021-01-27 11:21:22 +0100

    Init (Kotlin)

---
+++ src/test/kotlin/TestMain.kt
@@ -0,0 +1,11 @@
+import com.codingame.gameengine.runner.MultiplayerGameRunner
+
+object TestMain {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val gameRunner = MultiplayerGameRunner()
+        gameRunner.addAgent(Agent1::class.java)
+        gameRunner.addAgent(Agent2::class.java)
+        gameRunner.start()
+    }
+}
